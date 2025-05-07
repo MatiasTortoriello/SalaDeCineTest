dc76ae133bfa843dd4a6ae60d96891223fc6f54e
MatiasTortoriello <matiastortoriello1991@gmail.com>
2025-05-07 04:55:01 -0300

    Atributos

--- src/salaDeCineDominio/SalaDeCine.java
+++ src/salaDeCineDominio/SalaDeCine.java
@@ -1,5 +1,12 @@
 package salaDeCineDominio;
 
 public class SalaDeCine {
-
+	
+    private int[][] asientos;
+    private int filas;
+    private int columnas;
+    
+	public SalaDeCine() {
+	
+	}
 }
